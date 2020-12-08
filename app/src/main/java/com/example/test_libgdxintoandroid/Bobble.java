63f97fc09ad3097349388dbb63ffcb8c8e1dbc90
Alexandre Zanni <zanni.ad.dev.web@gmail.com>
2020-12-09 00:17:43 +0100

    Adding the base project of a libGDX activity (hello world) into an Android project

---
+++ app/src/main/java/com/example/test_libgdxintoandroid/Bobble.java
@@ -0,0 +1,178 @@
+package com.example.test_libgdxintoandroid;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Pixmap.Format;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas.AtlasRegion;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Box2D;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton.TextButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
+import com.badlogic.gdx.utils.Array;
+import com.badlogic.gdx.utils.viewport.ExtendViewport;
+
+import java.util.HashMap;
+
+class MyGdxGame extends ApplicationAdapter {
+    final HashMap<String, Sprite> sprites = new HashMap<String, Sprite>();
+    TextureAtlas textureAtlas;
+    SpriteBatch batch;
+    OrthographicCamera camera;
+    ExtendViewport viewport;
+    World world;
+    Skin skin;
+    Stage stage;
+    static final float STEP_TIME = 1f / 60f;
+    static final int VELOCITY_ITERATIONS = 6;
+    static final int POSITION_ITERATIONS = 2;
+
+    float accumulator = 0;
+
+
+    // Define an interface for your various callbacks to the android launcher
+    public interface MyGameCallback {
+        public void onStartActivityA();
+        public void onStartActivityB();
+        public void onStartSomeActivity(int someParameter, String someOtherParameter);
+    }
+
+    // Local variable to hold the callback implementation
+    private MyGameCallback myGameCallback;
+
+    // ** Additional **
+    // Setter for the callback
+    public void setMyGameCallback(MyGameCallback callback) {
+        myGameCallback = callback;
+    }
+
+    @Override
+    public void create () {
+        batch = new SpriteBatch();
+        camera = new OrthographicCamera();
+        stage = new Stage();
+        viewport = new ExtendViewport(800, 600, camera);
+        textureAtlas = new TextureAtlas("sprites.txt");
+        addSprites();
+        Box2D.init();
+        world = new World(new Vector2(0, -10), true);
+        Gdx.input.setInputProcessor(stage);
+
+        // A skin can be loaded via JSON or defined programmatically, either is fine. Using a skin is optional but strongly
+        // recommended solely for the convenience of getting a texture, region, etc as a drawable, tinted drawable, etc.
+        skin = new Skin();
+        // Generate a 1x1 white texture and store it in the skin named "white".
+        Pixmap pixmap = new Pixmap(1, 1, Format.RGBA8888);
+        pixmap.setColor(Color.WHITE);
+        pixmap.fill();
+        skin.add("white", new Texture(pixmap));
+
+        // Store the default libgdx font under the name "default".
+        skin.add("default", new BitmapFont());
+
+        // Configure a TextButtonStyle and name it "default". Skin resources are stored by type, so this doesn't overwrite the font.
+        TextButtonStyle textButtonStyle = new TextButtonStyle();
+        textButtonStyle.up = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.down = skin.newDrawable("white", Color.DARK_GRAY);
+        textButtonStyle.checked = skin.newDrawable("white", Color.BLUE);
+        textButtonStyle.over = skin.newDrawable("white", Color.LIGHT_GRAY);
+        textButtonStyle.font = skin.getFont("default");
+        skin.add("default", textButtonStyle);
+
+        // Create a table that fills the screen. Everything else will go inside this table.
+        Table table = new Table();
+        table.setFillParent(true);
+        stage.addActor(table);
+
+        // Create a button with the "default" TextButtonStyle. A 3rd parameter can be used to specify a name other than "default".
+        final TextButton button = new TextButton("Click me!", skin);
+        table.add(button).size(300);
+
+        // Add a listener to the button. ChangeListener is fired when the button's checked state changes, eg when clicked,
+        // Button#setChecked() is called, via a key press, etc. If the event.cancel() is called, the checked state will be reverted.
+        // ClickListener could have been used, but would only fire when clicked. Also, canceling a ClickListener event won't
+        // revert the checked state.
+        button.addListener(new ChangeListener() {
+            public void changed (ChangeEvent event, Actor actor) {
+                System.out.println("Clicked! Is checked: " + button.isChecked());
+                button.setText("Good job!");
+            }
+        });
+        // Add an image actor. Have to set the size, else it would be the size of the drawable (which is the 1x1 texture).
+        table.add(new Image(skin.newDrawable("white", Color.RED))).size(300);
+
+    }
+
+    private void addSprites() {
+        Array<AtlasRegion> regions = textureAtlas.getRegions();
+
+        for (AtlasRegion region : regions) {
+            Sprite sprite = textureAtlas.createSprite(region.name);
+            sprites.put(region.name, sprite);
+        }
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+        viewport.update(width, height, true);
+        batch.setProjectionMatrix(camera.combined);
+    }
+
+    private void stepWorld() {
+        float delta = Gdx.graphics.getDeltaTime();
+
+        accumulator += Math.min(delta, 0.25f);
+
+        if (accumulator >= STEP_TIME) {
+            accumulator -= STEP_TIME;
+
+            world.step(STEP_TIME, VELOCITY_ITERATIONS, POSITION_ITERATIONS);
+        }
+    }
+
+    @Override
+    public void render () {
+        stepWorld();
+        Gdx.gl.glClearColor(0.57f, 0.77f, 0.85f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+        batch.begin();
+
+        drawSprite("banana", 0, 0);
+        drawSprite("cherries", 100, 100);
+
+        batch.end();
+    }
+
+    private void drawSprite(String name, float x, float y) {
+        Sprite sprite = sprites.get(name);
+        sprite.setPosition(x, y);
+        sprite.draw(batch);
+    }
+
+    @Override
+    public void dispose () {
+        stage.dispose();
+        skin.dispose();
+        textureAtlas.dispose();
+        sprites.clear();
+        world.dispose();
+    }
+}
---
+++ app/src/main/java/com/example/test_libgdxintoandroid/MainActivity.java
@@ -0,0 +1,66 @@
+/*package com.example.test_libgdxintoandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        try
+        {
+            setContentView(R.layout.activate_screen);
+            Button b_Run = (Button) findViewById(id.bActiveRun);
+
+            b_Run.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent to_Bobble = new Intent(v.getContext(), Bobble.class);
+                    startActivity(to_Bobble);
+                }
+            });
+        }
+        catch (Exception e)
+        {
+            Log.e("Activate", "Error in activity", e);
+
+            Toast.makeText(getApplicationContext(),
+                    e.getClass().getName() + " " + e.getMessage(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+    /*
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(navView, navController);
+    }*/
+
+//}
